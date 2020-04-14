89d8687d61b2794398014553379daa9f5fa5ff23
shadiqfauzi <60591553+shadiqfauzi@users.noreply.github.com>
2020-04-14 22:07:09 +0700

    Login & Register UI prototype
    dan tweak header

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Input, Button, Label } from 'reactstrap'
 
-class Register extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>
-                
+const Register = () => {
+    return (
+        <div className='container-fluid'>
+            <div className='row'>
+                    <div className='col-md-8 img-placeholder'>
+                </div>
+                <div className='col-md-4 d-flex flex-column pr-5 pl-5 auth-form'>
+                    <h1>Register</h1>
+                    <Label for='username' className='mt-5'>Username</Label>
+                    <Input type='text' placeholder='Username' id='username'></Input>
+                    <Label for='email' className='mt-2'>Email</Label>
+                    <Input type='email' placeholder='Email' id='email'></Input>
+                    <Label for='password' className='mt-2'>Password</Label>
+                    <Input type='password' placeholder='Password' id='password'></Input>
+                    <Label for='confirmPassword' className='mt-2'>Confirm Password</Label>
+                    <Input type='password' placeholder='Confirm Password' id='confirmPassword'></Input>
+                    <Button className='mt-5 auth-button' color='success'>Register Now</Button>
+                </div>
             </div>
-         );
-    }
+        </div>
+    );
 }
  
-export default Register
+export default Register;
