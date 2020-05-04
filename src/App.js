8249e613b277a5db56abfa2668adaf679e5c7964
shadiqfauzi <60591553+shadiqfauzi@users.noreply.github.com>
2020-05-04 21:37:59 +0700

    CRUD Manage Product v1
    Belom selesai ini

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React from 'react'
 // import logo from './logo.svg';
-import './App.css';
+import './App.css'
 import { Route, Switch } from 'react-router-dom'
-import LandingPage from './Pages/LandingPage'
-import Login from './Pages/Login'
-import Register from './Pages/Register'
+
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 
+import LandingPage from './Pages/LandingPage'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import ManageProductPage from './Pages/ManageProductPage'
+import AddProductPage from './Pages/AddProductPage'
+
 function App() {
-  return (
-    <div>
-        <Header/>
-        <Switch>
-          <Route path='/' component={LandingPage} exact />
-          <Route path='/login' component={Login} />
-          <Route path='/register' component={Register} />
-        </Switch>
-        <Footer/>
-      </div>
-  );
+	return (
+		<div>
+			<Header />
+			<Switch>
+				<Route path='/' component={LandingPage} exact />
+				<Route path='/login' component={Login} />
+				<Route path='/register' component={Register} />
+				<Route path='/manage-product' component={ManageProductPage} />
+				<Route path='/add-product' component={AddProductPage} />
+			</Switch>
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
