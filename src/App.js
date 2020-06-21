e42591650822321526bfca6fc88c8a6fb65f299e
artharf <artharf@gmail.com>
2020-06-21 20:44:24 +0700

    Puja Kerang ajaib
    wawaw wiwawaw lala wiwiw awawwawa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 // import logo from './logo.svg';
 import './App.css'
 import { Route, Switch } from 'react-router-dom'
@@ -7,8 +7,22 @@ import Login from './Pages/Login'
 import Register from './Pages/Register'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import Profile from './Pages/Profile'
+import {keepLogin} from './Redux/Action/authAction'
+import { useDispatch } from 'react-redux'
+import TransHistory from './Pages/TransHistory'
+import editProfile from './Pages/EditProfile'
+import ManageUsers from './Pages/ManageUsers'
 
 function App() {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+      dispatch(
+        keepLogin()
+      )
+    })
+
     return (
         <div>
             <Header />
@@ -16,6 +30,10 @@ function App() {
                 <Route path='/' component={LandingPage} exact />
                 <Route path='/login' component={Login} />
                 <Route path='/register' component={Register} />
+                <Route path='/profile' component={Profile} />
+                <Route path='/transhistory' component={TransHistory}/>
+                <Route path='/editProfile' component={editProfile} />
+                <Route path='/manageUsers' component={ManageUsers} />
             </Switch>
             <Footer />
         </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -12,12 +12,18 @@ import {
     DropdownToggle,
     DropdownMenu,
     DropdownItem,
-} from 'reactstrap'
+} from 'reactstrap';
+import { Logout } from '../Redux/Action/authAction';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Header = (props) => {
     const [isOpen, setIsOpen] = useState(false)
 
     const toggle = () => setIsOpen(!isOpen)
+
+    const user = useSelector((state) => state.auth.username)
+
+    const dispatch = useDispatch()
 
     return (
         <div>
@@ -55,14 +61,32 @@ const Header = (props) => {
                                 User
                             </DropdownToggle>
                             <DropdownMenu right>
-                                <Link to='/login'>
-                                    <DropdownItem>Login</DropdownItem>
-                                </Link>
-                                <Link to='/register'>
-                                    <DropdownItem>Register</DropdownItem>
-                                </Link>
-                                <DropdownItem divider />
-                                <DropdownItem>Reset</DropdownItem>
+                                {
+                                    user
+                                    ?
+                                    <React.Fragment>
+                                    <Link to='/profile'>
+                                        <DropdownItem>Profile</DropdownItem>
+                                    </Link>
+                                    <Link to='/transhistory'>
+                                        <DropdownItem>Transaction History</DropdownItem>
+                                    </Link>
+                                    <Link to='/' onClick={() => dispatch(Logout())}>
+                                        <DropdownItem>Logout</DropdownItem>
+                                    </Link>
+                                    </React.Fragment>
+                                    :
+                                    <React.Fragment>      
+                                    <Link to='/login'>
+                                        <DropdownItem>Login</DropdownItem>
+                                    </Link>
+                                    <Link to='/register'>
+                                        <DropdownItem>Register</DropdownItem>
+                                    </Link>
+                                    <DropdownItem divider />
+                                    <DropdownItem>Reset</DropdownItem>
+                                    </React.Fragment>
+                                }
                             </DropdownMenu>
                         </UncontrolledDropdown>
                     </Nav>
